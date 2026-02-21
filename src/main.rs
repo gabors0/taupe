2313ef96080c18fea0afaa8e486b8da10721d6d2
gabors0 <gabors0@proton.me>
2026-02-21 11:07:46 +0100

    update: iced and rfd

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,33 @@ mod audio;
 mod gui;
 
 use audio::spawn_audio_thread;
-use iced::Color;
 use iced::theme::Palette;
+use iced::Color;
 
-fn custom_palette() -> Palette {
+const fn custom_palette() -> Palette {
     Palette {
         background: Color::from_rgb(0.12, 0.11, 0.10),
         text: Color::from_rgb(0.88, 0.86, 0.84),
         primary: Color::from_rgb(0.702, 0.576, 0.463),
         success: Color::from_rgb(0.48, 0.54, 0.41),
+        warning: Color::from_rgb(0.80, 0.60, 0.30),
         danger: Color::from_rgb(0.64, 0.36, 0.31),
     }
 }
 
+fn theme_fn(_state: &gui::App) -> iced::Theme {
+    iced::Theme::custom("Custom Dark", custom_palette())
+}
+
 fn main() -> iced::Result {
     let (audio_cmd, _audio_handle) = spawn_audio_thread();
-    let theme = iced::Theme::custom("Custom Dark".into(), custom_palette());
 
-    iced::application("Taupe", gui::update, gui::view)
-        .theme(move |_| theme.clone())
-        .run_with(|| (gui::App::new(audio_cmd), iced::Task::none()))
+    iced::application(
+        move || (gui::App::new(audio_cmd.clone()), iced::Task::none()),
+        gui::update,
+        gui::view,
+    )
+    .title("Taupe")
+    .theme(theme_fn)
+    .run()
 }
